ce79e67cc219f4238b06cf3b4322a0b26dd35d9e
bluewhale36 <kmsuhyn36@gmail.com>
2026-01-23 13:29:33 +0900

    Implementing pagination logic

--- src/main/java/com/example/spreadsheetdemo/herb/controller/HerbLogRestController.java
+++ src/main/java/com/example/spreadsheetdemo/herb/controller/HerbLogRestController.java
@@ -1,12 +1,14 @@
 package com.example.spreadsheetdemo.herb.controller;
 
 import com.example.spreadsheetdemo.herb.domain.HerbLogPagination;
-import com.example.spreadsheetdemo.herb.dto.HerbLogViewDTO;
 import com.example.spreadsheetdemo.herb.service.HerbService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @Slf4j
 @RestController
@@ -16,10 +18,10 @@ public class HerbLogRestController {
 
     private final HerbService herbService;
 
-//    @GetMapping("/{endRowNum}")
-//    public ResponseEntity<HerbLogPagination> herbLog(@PathVariable int endRowNum) {
-//        HerbLogPagination pagination = herbService.getHerbLogs(endRowNum);
-//        System.out.println(pagination.getData().toString());
-//        return ResponseEntity.ok(pagination);
-//    }
+    @GetMapping("/{date}")
+    public ResponseEntity<HerbLogPagination> getLogs(
+            @PathVariable(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date
+    ) {
+        return ResponseEntity.ok(herbService.getHerbLogs(date));
+    }
 }
